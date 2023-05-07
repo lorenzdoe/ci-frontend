04a9757e3d6f0ea0adff61f0cef630182465b476
lorenz <lorenz.doe@hotmail.com>
2023-05-07 17:45:38 +0200

    authorization headers for todos

--- src/api.js
+++ src/api.js
@@ -1,7 +1,21 @@
 import axios from "axios";
 
+// TODO:
+// create register function (POST to http://localhost:3000/users)
+
+// create login function (POST to http://localhost:3000/sessions)
+// and store the token in localStorage
+
+// access the token from local storage and include it in the headers
 const readTodos = async () => {
-  let res = await axios.get('http://localhost:3000/todos');
+  let res = await axios.get(
+        'http://localhost:3000/todos',
+        {
+            headers: {
+                Authorization: 'Baerer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBhcGFzY2hsdW1wZiIsImlhdCI6MTY4MzQ2NzM0NywiZXhwIjoxNjgzNDcwOTQ3fQ.fOFr1KRFrpsvSiLzoh4lbA3xf7-fHrN0aywhOITsPVQ'
+            }
+        }
+    );
 
   return res.data;
 }
@@ -11,6 +25,11 @@ const createTodo = async (name) => {
         'http://localhost:3000/todos',
         {
             'name': name
+        },
+        {
+            headers: {
+                Authorization: 'Baerer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBhcGFzY2hsdW1wZiIsImlhdCI6MTY4MzQ2NzM0NywiZXhwIjoxNjgzNDcwOTQ3fQ.fOFr1KRFrpsvSiLzoh4lbA3xf7-fHrN0aywhOITsPVQ'
+            }
         }
     );
 
@@ -19,7 +38,13 @@ const createTodo = async (name) => {
 
 const doneTodo = async (id) => {
     let res = await axios.put(
-        `http://localhost:3000/todos/${id}/done`
+        `http://localhost:3000/todos/${id}/done`,
+        null,
+        {
+            headers: {
+                Authorization: 'Baerer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBhcGFzY2hsdW1wZiIsImlhdCI6MTY4MzQ2NzM0NywiZXhwIjoxNjgzNDcwOTQ3fQ.fOFr1KRFrpsvSiLzoh4lbA3xf7-fHrN0aywhOITsPVQ'
+            }
+        }
     );
 
     return res.data;
@@ -27,7 +52,12 @@ const doneTodo = async (id) => {
 
 const undoneTodo = async (id) => {
     let res = await axios.delete(
-        `http://localhost:3000/todos/${id}/done`
+        `http://localhost:3000/todos/${id}/done`,
+        {
+            headers: {
+                Authorization: 'Baerer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBhcGFzY2hsdW1wZiIsImlhdCI6MTY4MzQ2NzM0NywiZXhwIjoxNjgzNDcwOTQ3fQ.fOFr1KRFrpsvSiLzoh4lbA3xf7-fHrN0aywhOITsPVQ'
+            }
+        }
     );
 
     return res.data;
